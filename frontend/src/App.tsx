0d858b3a64d29f1a28336a407fa5b1e689cb19ab
hiagovinicius0 <hiago149@gmail.com>
2020-09-03 21:06:09 -0300

    Página de Login

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SigIn from './pages/sigin'
+import GlobalStyle from './styles/global'
+
+const App: React.FC = () => (
+    <>
+        <SigIn />
+        <GlobalStyle />
+    </>
+)
+
+export default App;
